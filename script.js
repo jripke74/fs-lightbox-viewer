a040834abae8652997cac4c0ee19e65a65da064e
Jeff Ripke <jripke74@gmail.com>
2025-04-02 09:13:27 -0700

    add addEventListner()

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const galleryItem = document.querySelector(".gallery-item");
+const galleryItems = document.querySelectorAll(".gallery-item");
 const lightbox = document.querySelector(".lightbox");
+const lightboxImage = document.querySelector("#lightbox-image");
+const closeButton = document.querySelector("#close");
 
-galleryItem.addEventListener("click", function () {
+function itemClick(event) {
   lightbox.style.display = "flex";
+  lightboxImage.src = event.target.src.replace("-thumbnail", "");
+  event.stopPropagation();
+}
+
+galleryItems.forEach((galleryItem) => {
+  galleryItem.addEventListener("click", itemClick);
+});
+
+closeButton.addEventListener("click", (event) => {
+  if (lightbox.style.display === "flex" && event.target !== lightboxImage) {
+    lightbox.style.display = "none";
+  }
+});
+
+lightbox.addEventListener("click", (event) => {
+  if (lightbox.style.display === "flex" && event.target !== lightboxImage) {
+    lightbox.style.display = "none";
+  }
 });
